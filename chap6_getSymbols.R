0e5dffebf53c5f6aa65e5759b940563e83e90c7e
wangshuimail <wangshuimail@gmail.com>
2014-08-03 04:51:31 +0000

    panel.hist()应该放到脚本前部。

--- chap6_getSymbols.R
+++ chap6_getSymbols.R
@@ -1,3 +1,16 @@
+####chap6_getSymbols.R
+####helper function:
+panel.hist <- function(x, ...)
+{
+  usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(usr[1:2], 0, 1.5) )
+  h <- hist(x, plot = FALSE)
+  breaks <- h$breaks; nB <- length(breaks)
+  y <- h$counts; y <- y/max(y)
+  rect(breaks[-nB], 0, breaks[-1], y, col = "cyan", ...)
+}
+
+####
 getSymbols("000538.SZ", from="2004-1-1")
 getSymbols("600336.SS", from="2004-1-1")
 getSymbols("600337.SS", from="2004-1-1")
@@ -9,13 +22,3 @@ colnames(mydata)=c('ynby', 'aokm', 'mkjj')
 View(mydata)
 pairs(mydata[,(1:3)], diag.panel=panel.hist)
 
-####
-panel.hist <- function(x, ...)
-{
-  usr <- par("usr"); on.exit(par(usr))
-  par(usr = c(usr[1:2], 0, 1.5) )
-  h <- hist(x, plot = FALSE)
-  breaks <- h$breaks; nB <- length(breaks)
-  y <- h$counts; y <- y/max(y)
-  rect(breaks[-nB], 0, breaks[-1], y, col = "cyan", ...)
-}
